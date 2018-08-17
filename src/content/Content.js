a71be4529204b2b50dac03ecd286c4b7c215aee9
Joel Aro <joelaro.to@gmail.com>
2018-08-17 12:58:24 -0400

    Modified bio

--- src/content/Content.js
+++ src/content/Content.js
@@ -5,7 +5,8 @@ class Content extends Component {
 	render() {
 		return (
 			<div className="content">
-				<p>My name is Joel Aro, and I am a computer programming sorcerer currently living in Mississauga, Ontario. Currently interning as a Desktop Systems Specialist at Mozilla Corporation. I am an advocate for the Free and Open Source Software movement and protection of the Free Web.</p>
+				<p>My name is Joel Aro, and I am a computer programming sorcerer currently living in Mississauga, Ontario.</p>
+				<p>In 2018, I was an intern at Mozilla as a Desktop Systems Specialist, and provided technical support for remote employees and contributors. I am an advocate for the Free and Open Source Software movement and protection of the Free Web.</p>
 				<p>When I am not coding, I enjoy playing Dungeons & Dragons, Magic: The Gathering, and various online games.</p>
 			</div>
 		);
